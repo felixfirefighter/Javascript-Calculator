75751a770d1e185be4c03fc11b9e3cabae60a32c
YoongTi <leeyoongti@gmail.com>
2016-10-19 00:14:19 -0400

    Added gradient to buttons and some color varieties

--- script.js
+++ script.js
@@ -2,17 +2,82 @@
  * Created by LEE on 2016/10/18.
  */
 
-$(document).ready(function () {
-    var currentInput = $("#current-input");
+function isTooLong() {
+    return $("#current-input").text().length > 17;
+}
 
-    currentInput.change(function () {
-       alert(this.length);
-        if(this.length > 17){
-           this.text("");
-       }
+function appendInput(value){
+        if(isTooLong()){
+            $("#current-input").text("");
+            $("#result").text("Max Digit Input!");
+        }else{
+            $("#current-input").append(value);
+            $("#result").text("");
+        }
+
+}
+
+$(document).ready(function () {
+    $("#one").click(function () {
+       appendInput("1");
+    });
+
+    $("#two").click(function () {
+        appendInput("1");
+    });
+
+    $("#three").click(function () {
+        appendInput("1");
+    });
+
+    $("#four").click(function () {
+        appendInput("1");
+    });
+    $("#five").click(function () {
+        appendInput("1");
+    });
+
+    $("#six").click(function () {
+        appendInput("1");
+    });
+
+    $("#seven").click(function () {
+        appendInput("1");
+    });
+
+    $("#eight").click(function () {
+        appendInput("1");
+    });
+    $("#nine").click(function () {
+        appendInput("1");
+    });
+
+    $("#dot").click(function () {
+        appendInput("1");
+    });
+
+    $("#plus").click(function () {
+        appendInput("1");
+    });
+
+    $("#minus").click(function () {
+        appendInput("1");
+    });
+
+    $("#divide").click(function () {
+        appendInput("1");
+    });
+
+    $("#").click(function () {
+        appendInput("1");
     });
 
     $("#one").click(function () {
-        currentInput.append("1");
+        appendInput("1");
     });
+
+    $("#one").click(function () {
+        appendInput("1");
+    });
+
 });
